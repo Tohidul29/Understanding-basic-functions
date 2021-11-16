d4b9cb4697671031f24f6d07d8195aac3bd0087a
MD TOHIDUL ISLAM <64440287+Tohidul29@users.noreply.github.com>
2021-11-16 20:26:57 +0600

    Add files via upload

---
+++ basic_function_idea.py
@@ -0,0 +1,20 @@
+#for the square:
+def area_of_square(a):
+    return(a*a)
+
+#for the circle:
+def area_of_circle(a):
+    return(3.1416*(a*a))
+
+#for the triangle:
+def area_of_triangle(a,b):
+    return(0.5*(a*b))
+
+#for the rectangle:
+def area_of_rectangle(a,b):
+    return(a*b)
+
+print(area_of_square(5))
+print(area_of_circle(5))
+print(area_of_triangle(5,3))
+print(area_of_rectangle(5,4))
